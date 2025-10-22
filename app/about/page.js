545167b3c37395badbe0e56a9cf0e4b14579f5fc
mdasifalijihat <mdasifalijihat@gmail.com>
2025-10-22 23:41:21 +0600

    added about

--- app/about/page.js
+++ app/about/page.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>page
+      <h1>About Us</h1>
+      <p>Welcome to our e-commerce platform! We are dedicated to providing you with the best online shopping experience. Our mission is to offer a wide range of products at competitive prices, along with exceptional customer service.</p>
+      <p>Founded in 2023, our company has quickly grown to become a trusted name in the e-commerce industry. We believe in quality, affordability, and convenience, and we strive to exceed our customers' expectations every day.</p>
+      <p>Thank you for choosing us for your shopping needs. We look forward to serving you!</p>
+      
+    </div>
   )
 }
